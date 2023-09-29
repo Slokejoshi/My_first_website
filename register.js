9b5d31eab95f7cb655c3fbc32c5f559fd1315d7f
Slokejoshi <135275620+Slokejoshi@users.noreply.github.com>
2023-09-29 10:52:43 -0400

    fixed alert_pop-up

--- register.js
+++ register.js
@@ -15,11 +15,12 @@
     console.log("Lastname =", lName.value);
     console.log("Email =", email.value);
     console.log("password =", orgPassword.value);
+
+    if (orgPassword.value != cnfPassword.value) {
+      alert("Password and Confirm password should be same");
+    }
   });
 
-  // if (orgPassword != cnfPassword) {
-  //   alert("Password and Confirm password should be same");
-  // }
 
   Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener(
